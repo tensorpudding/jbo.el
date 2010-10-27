bf3f88b973556516983813c7dd1806811ecd4afc
Michael Moorman <tensorpuddin@devio.us>
2010-10-26 23:22:09 -0400

    History searching, cross-references work.

--- jbo.el
+++ jbo.el
@@ -1,199 +1,38 @@
 ;; jbo.el --- jbo integration with Emacs
 
 (provide 'jbo)
-
 (add-to-list 'auto-mode-alist '("*jbo*" . jbo-dict-mode))
 
-;;; jbo faces
+(defun jbo-prettifier (entry)
+  "Prettifies the jbo dictionary entry"
+  (let* ((index 0)
+	 (lede-regex "^[a-zA-Z']+")
+	 (link-regex "{[a-zA-Z']+}")
+	 (text (replace-regexp-in-string lede-regex 'jbo-ledefier entry)))
+    (replace-regexp-in-string link-regex 'jbo-linkfier text)))
 
-(defgroup jbo nil
-  "jbo interaction"
-  :prefix "jbo-")
+(defun jbo-ledefier (word)
+  (propertize word 'face 'bold))
 
-(defface jbo-definition
-  (let ((font (cond ((assq :inherit custom-face-attributes)))))
-    '((((type tty) (class color))
-       (:foreground "white"))
-      (((class grayscale) (background light))
-       (:foreground "black" ,@font))
-      (((class grayscale) (background dark))
-       (:foreground "white" ,@font))
-      (((class color) (background light))
-       (:foreground "black"))
-      (((class color) (background dark))
-       (:foreground "white"))
-      (t (,@font))))
-  "Face used for valsi definition body."
-  :group 'jbo-dict-faces)
+(defun jbo-linkfier (word)
+  (propertize (substring word 1 -1)
+	      'face 'link
+;	      'mouse-face 'highlight
+;	      'follow-link t
+;	      'help-echo "mouse-1: look up this valsi"
+	      'keymap jbo-link-keymap))
 
-(defface jbo-cmene
-  (let ((font (cond ((assq :inherit custom-face-attributes) '(:inherit bold))
-		    ((assq :weight custom-face-attributes) '(:weight bold))
-		    (t '(:bold t)))))
-      '((((class grayscale) (background light))
-	 (:foreground "black" ,@font))
-	(((class grayscale) (background dark))
-	 (:foreground "white" ,@font))
-	(((class color) (background light))
-	 (:foreground "#993b31"))
-	(((class color) (background dark))
-	 (:foreground "#ed8274"))
-	(t (,@font))))
-  "Face used for cmene definition lead."
-  :group 'jbo-dict-faces)
-
-(defface jbo-cmavo
-  (let ((font (cond ((assq :inherit custom-face-attributes) '(:inherit bold))
-		    ((assq :weight custom-face-attributes) '(:weight bold))
-		    (t '(:bold t)))))
-      '((((class grayscale) (background light))
-	 (:foreground "black" ,@font))
-	(((class grayscale) (background dark))
-	 (:foreground "white" ,@font))
-	(((class color) (background light))
-	 (:foreground "#318f99"))
-	(((class color) (background dark))
-	 (:foreground "#74dfed"))
-	(t (,@font))))
-  "Face used for cmavo definition lead."
-  :group 'jbo-dict-faces)
-
-(defface jbo-gismu
-  (let ((font (cond ((assq :inherit custom-face-attributes) '(:inherit bold))
-		    ((assq :weight custom-face-attributes) '(:weight bold))
-		    (t '(:bold t)))))
-      '((((class grayscale) (background light))
-	 (:foreground "black" ,@font))
-	(((class grayscale) (background dark))
-	 (:foreground "white" ,@font))
-	(((class color) (background light))
-	 (:foreground "#5a9931"))
-	(((class color) (background dark))
-	 (:foreground "#a3ed74"))
-	(t (,@font))))
-  "Face used for gismu definition lead."
-  :group 'jbo-dict-faces)
-
-(defface jbo-lujvo
-  (let ((font (cond ((assq :inherit custom-face-attributes) '(:inherit bold))
-		    ((assq :weight custom-face-attributes) '(:weight bold))
-		    (t '(:bold t)))))
-      '((((class grayscale) (background light))
-	 (:foreground "black" ,@font))
-	(((class grayscale) (background dark))
-	 (:foreground "white" ,@font))
-	(((class color) (background light))
-	 (:foreground "#6f3199"))
-	(((class color) (background dark))
-	 (:foreground "#bf74ed"))
-	(t (,@font))))
-  "Face used for gismu definition lead."
-  :group 'jbo-dict-faces)
-
-(defface jbo-cmavo-class
-  (let ((font (cond ((assq :inherit custom-face-attributes) '(:inherit italic))
-		    ((assq :weight custom-face-attributes) '(:weight italic))
-		    (t '(:bold t)))))
-      '((((class grayscale) (background light))
-	 (:foreground "black" ,@font))
-	(((class grayscale) (background dark))
-	 (:foreground "white" ,@font))
-	(((class color) (background light))
-	 (:foreground "#993150"))
-	(((class color) (background dark))
-	 (:foreground "#ed7496"))
-	(t (,@font))))
-  "Face used for cmavo classifications."
-  :group 'jbo-dict-faces)
-
-
-(defface jbo-link
-  (let ((font (cond ((assq :inherit custom-face-attributes) '(:inherit italic))
-		    ((assq :weight custom-face-attributes) '(:weight italic))
-		    (t '(:bold t)))))
-      '((((class grayscale) (background light))
-	 (:foreground "black" ,@font))
-	(((class grayscale) (background dark))
-	 (:foreground "white" ,@font))
-	(((class color) (background light))
-	 (:foreground "black"))
-	(((class color) (background dark))
-	 (:foreground "white"))
-	(t (,@font))))
-  "Face used for valsi cross-references."
-  :group 'jbo-dict-faces)
-
-;;; jbo prettifiers
-
-(defun jbo-text-former (valsi)
-  (let* ((type (jbo-get-type valsi))
-	(class (jbo-get-cmavo-class valsi))
-	(definition (jbo-get-definition valsi))
-	(notes (jbo-get-notes valsi))
-	(lede (jbo-lede-former valsi type class)))
-    (concat lede definition "\n" notes)))
-
-(defun jbo-lede-former (valsi type class)
-  "Returns the lede string \"VALSI (CLASS):\" with proper face properties added."
-  (cond ((string= "cmavo" type)
-	 (concat (propertize valsi 'face 'jbo-cmavo)
-		 " ("
-		 (propertize class 'face 'jbo-cmavo-class)
-		 "): "))
-	((string= "gismu" type)
-	 (concat (propertize valsi 'face 'jbo-gismu)
-		 ": "))
-	((string= "lujvo" type)
-	 (concat (propertize valsi 'face 'jbo-lujvo)
-		 ": "))
-	((string= "cmene" type)
-	 (concat (propertize valsi 'face 'jbo-cmene)
-		 ": "))
-	(t
-	 (concat (propertize valsi 'face 'jbo-definition)
-		 ":"))))
-
-(defun jbo-linkifier ()
-  (let ((index 0)
-	(link-regex "{\\([a-zA-Z']+\\)}"))
-    (while (numberp (string-match link-regex (buffer-string) index))
-      (make-button (+ 1 (match-beginning 1)) (+ 1 (match-end 1))
-			     '(help-echo "mouse-1: lookup this valsi"
-			       follow-link t
-			       mouse-face highlight
-			       action jbo-follow-link
-			       face jbo-link))
-      (setq index (match-end 0)))))
+(defvar jbo-link-keymap
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "RET") 'jbo-lookup-at-point)
+;;    (define-key map [Mouse-2] 'jbo-follow-link-with-mouse)
+    map))
 
 ;;; jbo backend querying
 
 (defun jbo-get-definition (valsi)
   "Query the jbo database for the definition of VALSI. If the definition exists return it, otherwise yield an error."
-  (let* ((command-string (concat "jbo get -d \"" valsi "\""))
-	(result (shell-command-to-string* command-string)))
-    (if (string-prefix-p "error:" result)
-	(error (concat valsi ": not found"))
-      result)))
-
-(defun jbo-get-type (valsi)
-  "Query the jbo database for the type of VALSI. If it exists, return it, otherwise yield an error."
-  (let* ((command-string (concat "jbo get -t \"" valsi "\""))
-	(result (shell-command-to-string* command-string)))
-    (if (string-prefix-p "error:" result)
-	(error (concat valsi ": not found"))
-      result)))
-
-(defun jbo-get-cmavo-class (cmavo)
-  "Query the jbo database for the class of CMAVO. If CMAVO is not a cmavo, returns the empty string, otherwise returns the class."
-  (let* ((command-string (concat "jbo get -c \"" cmavo "\""))
-	 (result (shell-command-to-string* command-string)))
-    (if (string-prefix-p "error:" result)
-	(error (concat cmavo ": not found"))
-      result)))
-
-(defun jbo-get-notes (valsi)
-  "Query the jbovlaste database for notes attached to VALSI. If it exists, return it, otherwise yield an error."
-  (let* ((command-string (concat "jbo get -n \"" valsi "\""))
+  (let* ((command-string (concat "jbo define \"" valsi "\""))
 	(result (shell-command-to-string* command-string)))
     (if (string-prefix-p "error:" result)
 	(error (concat valsi ": not found"))
@@ -205,20 +44,22 @@
   "Cut off that damn newline at the end"
   (substring (shell-command-to-string command-string) 0 -1))
   
-
-(defun jbo-lookup ()
+(defun jbo-lookup (name)
   "Prompt user for a valsi to look up, then returns the definition in the minibuffer *jbo*."
-  (interactive)
-  (jbo-lookup-definition (read-from-minibuffer "Valsi to look up: ")))
+  (interactive "sValsi to look up: ")
+  (when (or (not (stringp name))
+	    (string-equal "" name))
+    (error "Need to specify a word."))
+  (jbo-lookup-definition name))
 
-(defun jbo-follow-link (event)
-  "Cross-reference valsi under point in *jbo*."
-  (interactive "e")
-  (goto-char (posn-point (event-end event)))
-  (let ((link-regex "{\\([a-zA-Z']+\\)}")
-	(curr (word-at-point)))
-    (if (numberp (string-match link-regex curr))
-	(jbo-lookup-definition (replace-regexp-in-string link-regex "\\1" curr)))))
+;; (defun jbo-follow-link-with-mouse (event)
+;;   "Cross-reference valsi under point in *jbo*."
+;;   (interactive "e")
+;;   (let ((window (posn-window (event-end event)))
+;; 	(pos (posn-point (event-end event))))
+;;     (with-current-buffer (window-buffer window)
+;;       (goto-char pos)
+;;       (jbo-lookup-definition (word-at-point)))))
 
 (defun jbo-lookup-at-point ()
   "Lookup valsi at point"
@@ -226,33 +67,57 @@
   (jbo-lookup-definition (current-word)))
 
 (defun jbo-lookup-definition (valsi)
-  (let ((jbuff (get-buffer-create "*jbo*"))
-	(entry (jbo-text-former valsi)))
+  (let ((entry (jbo-get-definition valsi)))
     (if (not (string= "" entry))
-	(progn (pop-to-buffer jbuff)
-	       (setq buffer-read-only nil)
-	       (erase-buffer)
-	       (insert (jbo-text-former valsi))
-	       (fit-window-to-buffer)
-	       (jbo-linkifier)
-	       (jbo-dict-mode)))))
+	(progn (jbo-get-buffer-create)
+	       (jbo-display-definition (ring-insert jbo-history (jbo-prettifier entry)))))))
 
-(defvar jbo-dict-mode-link-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "RET") 'jbo-follow-link)
-    (define-key map [mouse-1] 'jbo-follow-link)
-    map))
+(defun jbo-get-buffer-create ()
+  (let ((jbuff (get-buffer "*jbo*")))
+    (if (not jbuff)
+	(progn (pop-to-buffer (get-buffer-create "*jbo*"))
+	       (jbo-dict-mode))
+      (pop-to-buffer jbuff))))
+
+(defun jbo-previous-definition ()
+  (interactive)
+  (jbo-get-buffer-create)
+  (ring-insert-at-beginning jbo-history (ring-remove jbo-history 0))
+  (jbo-display-definition (ring-ref jbo-history 0)))
+
+(defun jbo-next-definition ()
+  (interactive)
+  (jbo-get-buffer-create)
+  (ring-insert jbo-history (ring-remove jbo-history))
+  (jbo-display-definition (ring-ref jbo-history 0)))
+
+(defun jbo-display-definition (definition)
+  (setq buffer-read-only nil)
+  (erase-buffer)
+  (insert definition)
+  (fit-window-to-buffer)
+  (setq buffer-read-only t))
 
 (defvar jbo-dict-mode-map
     (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "q") 'delete-window)
+      (define-key map (kbd "q") 'delete-window)
+      (define-key map "\C-c\C-b" 'jbo-previous-definition)
+      (define-key map "\C-c\C-f" 'jbo-next-definition)
     map))
 
+(defvar jbo-max-history 100
+  "Maximum number of dictionary entries to remember")
+
 (defun jbo-dict-mode ()
-  "Mode for viewing word definitions from jbo."
+  "Major mode for viewing dictionary definitions from jbovlaste, the official Lojban dictionary. 
+Commands:
+\\{jbo-dict-mode-map}
+"
   (kill-all-local-variables)
+  (use-local-map jbo-dict-mode-map)
   (setq mode-name "jbo")
   (setq major-mode 'jbo-dict-mode)
-  (use-local-map jbo-dict-mode-map)
   (setq buffer-read-only t)
+  (make-local-variable 'jbo-history)
+  (setq jbo-history (make-ring jbo-max-history))
   (run-hooks 'jbo-mode-hook))
